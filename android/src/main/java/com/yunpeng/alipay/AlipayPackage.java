9331d57f0af65d3c8abda607ea3e8ef776759d30
lang <security_lang@foxmail.com>
2019-06-04 10:35:34 +0800

    修改打包问题

--- android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
+++ android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
@@ -21,11 +21,7 @@ public class AlipayPackage implements ReactPackage {
         modules.add(new AlipayModule(reactContext));
         return modules;
     }
-
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
+    
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
